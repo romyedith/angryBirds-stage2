3324618ed6bfbbea988605544a2f93695f30aea1
Romy Ocampo <romyedith@gmail.com>
2021-12-20 09:50:44 -0700

    se agregaron comentarios para plataforma

--- sketch.js
+++ sketch.js
@@ -18,7 +18,11 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    
+    //plataforma donde va ubicarse la resortera
     platform = new Ground(150,305,300,170);
+
+
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
